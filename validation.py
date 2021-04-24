93f4a5fc966e5650fb13b4d7e1720c7d0413a3dd
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-04-24 11:57:39 +0300

    update prompt

--- validation.py
+++ validation.py
@@ -7,7 +7,7 @@ def validate_car_id(cars_ids, car_id):
     while car_id not in cars_ids:
         try:
             car_id = int(
-                input("Enter TESTING Valid Car Id from List {}".format(cars_ids))
+                input("Enter Valid Car Id from List {}".format(cars_ids))
             )
         except ValueError:
             continue
